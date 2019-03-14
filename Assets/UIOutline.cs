4f9f7871ca0497070e285989d42263758b462e97
C.y <yangruihan@vip.qq.com>
2019-03-14 22:22:31 +0800

    添加代码及素材

---
+++ Assets/UIOutline.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+/// <summary>
+/// uGUI 描边
+/// </summary>
+public class UIOutline : BaseMeshEffect
+{
+    private static readonly List<UIVertex> Vertices = new List<UIVertex>();
+    private const AdditionalCanvasShaderChannels TangentShaderChannels = AdditionalCanvasShaderChannels.Tangent;
+
+    private static readonly int Color = Shader.PropertyToID("_OutlineColor");
+    private static readonly int Width = Shader.PropertyToID("_OutlineWidth");
+
+    // 描边颜色
+    [SerializeField] private Color outlineColor = UnityEngine.Color.white;
+
+    // 描边宽度
+    [SerializeField] [Range(0, 6)] private int outlineWidth = 0;
+
+    // 初始材质
+    [SerializeField] private Material originMaterial;
+
+    // 是否创建材质实例
+    [SerializeField] private bool createMaterialInstance = true;
+
+    private Material _material;
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+
+        Refresh();
+    }
+#endif
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (originMaterial == null)
+        {
+            var shader = Shader.Find("UI/Outline");
+            originMaterial = new Material(shader);
+        }
+
+        _material = createMaterialInstance ? Instantiate(originMaterial) : originMaterial;
+
+        var thisGraphic = graphic;
+        thisGraphic.material = _material;
+
+        var canvasAdditionalShaderChannels = thisGraphic.canvas.additionalShaderChannels;
+        if ((canvasAdditionalShaderChannels & TangentShaderChannels) != TangentShaderChannels)
+        {
+            graphic.canvas.additionalShaderChannels |= TangentShaderChannels;
+        }
+
+        Refresh();
+    }
+
+    public override void ModifyMesh(VertexHelper vh)
+    {
+        vh.GetUIVertexStream(Vertices);
+
+        ProcessVertices();
+
+        vh.Clear();
+        vh.AddUIVertexTriangleStream(Vertices);
+    }
+
+    private void Refresh()
+    {
+        if (_material == null)
+        {
+            if (originMaterial == null)
+            {
+                var shader = Shader.Find("UI/Outline");
+                originMaterial = new Material(shader);
+            }
+
+            _material = createMaterialInstance ? Instantiate(originMaterial) : originMaterial;
+        }
+
+        if (_material != null)
+        {
+            _material.SetColor(Color, outlineColor);
+            _material.SetInt(Width, outlineWidth);
+            graphic.SetVerticesDirty();
+        }
+    }
+
+    private void ProcessVertices()
+    {
+        for (int i = 0, count = Vertices.Count - 3; i <= count; i += 3)
+        {
+            var v1 = Vertices[i];
+            var v2 = Vertices[i + 1];
+            var v3 = Vertices[i + 2];
+
+            var minX = Min(v1.position.x, v2.position.x, v3.position.x);
+            var minY = Min(v1.position.y, v2.position.y, v3.position.y);
+            var maxX = Max(v1.position.x, v2.position.x, v3.position.x);
+            var maxY = Max(v1.position.y, v2.position.y, v3.position.y);
+            var posCenter = new Vector2(minX + maxX, minY + maxY) * 0.5f;
+
+            Vector2 triX, triY, uvX, uvY;
+            Vector2 pos1 = v1.position;
+            Vector2 pos2 = v2.position;
+            Vector2 pos3 = v3.position;
+            if (Mathf.Abs(Vector2.Dot((pos2 - pos1).normalized, Vector2.right))
+                > Mathf.Abs(Vector2.Dot((pos3 - pos2).normalized, Vector2.right)))
+            {
+                triX = pos2 - pos1;
+                triY = pos3 - pos2;
+                uvX = v2.uv0 - v1.uv0;
+                uvY = v3.uv0 - v2.uv0;
+            }
+            else
+            {
+                triX = pos3 - pos2;
+                triY = pos2 - pos1;
+                uvX = v3.uv0 - v2.uv0;
+                uvY = v2.uv0 - v1.uv0;
+            }
+
+            var uvMin = Min(v1.uv0, v2.uv0, v3.uv0);
+            var uvMax = Max(v1.uv0, v2.uv0, v3.uv0);
+            var uvOrigin = new Vector4(uvMin.x, uvMin.y, uvMax.x, uvMax.y);
+
+            v1 = UpdatePosAndUV(v1, outlineWidth, posCenter, triX, triY, uvX, uvY, uvOrigin);
+            v2 = UpdatePosAndUV(v2, outlineWidth, posCenter, triX, triY, uvX, uvY, uvOrigin);
+            v3 = UpdatePosAndUV(v3, outlineWidth, posCenter, triX, triY, uvX, uvY, uvOrigin);
+
+            Vertices[i] = v1;
+            Vertices[i + 1] = v2;
+            Vertices[i + 2] = v3;
+        }
+    }
+
+    private static UIVertex UpdatePosAndUV(
+        UIVertex vertex,
+        int outlineWidth,
+        Vector2 posCenter,
+        Vector2 triangleX,
+        Vector2 triangleY,
+        Vector2 uvx,
+        Vector2 uvy,
+        Vector4 uvOrigin)
+    {
+        var pos = vertex.position;
+        var posXOffset = pos.x > posCenter.x ? outlineWidth : -outlineWidth;
+        var posYOffset = pos.y > posCenter.y ? outlineWidth : -outlineWidth;
+        pos.x += posXOffset;
+        pos.y += posYOffset;
+        vertex.position = pos;
+
+        var uv = vertex.uv0;
+        uv += uvx / triangleX.magnitude * posXOffset * (Vector2.Dot(triangleX, Vector2.right) > 0 ? 1 : -1);
+        uv += uvy / triangleY.magnitude * posYOffset * (Vector2.Dot(triangleY, Vector2.up) > 0 ? 1 : -1);
+        vertex.uv0 = uv;
+
+        vertex.tangent = uvOrigin;
+
+        return vertex;
+    }
+
+    private static float Min(float a, float b, float c)
+    {
+        return Mathf.Min(Mathf.Min(a, b), c);
+    }
+
+    private static float Max(float a, float b, float c)
+    {
+        return Mathf.Max(Mathf.Max(a, b), c);
+    }
+
+    private static Vector2 Min(Vector2 a, Vector2 b, Vector2 c)
+    {
+        return new Vector2(Min(a.x, b.x, c.x), Min(a.y, b.y, c.y));
+    }
+
+    private static Vector2 Max(Vector2 a, Vector2 b, Vector2 c)
+    {
+        return new Vector2(Max(a.x, b.x, c.x), Max(a.y, b.y, c.y));
+    }
+}
---
+++ Assets/UIOutline.shader
@@ -0,0 +1,157 @@
+﻿Shader "UI/Outline"
+{
+    Properties
+    {
+        [PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+        _Color ("Tint", Color) = (1,1,1,1)
+        
+        _OutlineColor ("Outline Color", Color) = (1,1,1,1)
+        _OutlineWidth ("Outline Width", Int) = 1 
+
+        _StencilComp ("Stencil Comparison", Float) = 8
+        _Stencil ("Stencil ID", Float) = 0
+        _StencilOp ("Stencil Operation", Float) = 0
+        _StencilWriteMask ("Stencil Write Mask", Float) = 255
+        _StencilReadMask ("Stencil Read Mask", Float) = 255
+
+        _ColorMask ("Color Mask", Float) = 15
+
+        [Toggle(UNITY_UI_ALPHACLIP)] _UseUIAlphaClip ("Use Alpha Clip", Float) = 0
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "Queue"="Transparent"
+            "IgnoreProjector"="True"
+            "RenderType"="Transparent"
+            "PreviewType"="Plane"
+            "CanUseSpriteAtlas"="True"
+        }
+
+        Stencil
+        {
+            Ref [_Stencil]
+            Comp [_StencilComp]
+            Pass [_StencilOp]
+            ReadMask [_StencilReadMask]
+            WriteMask [_StencilWriteMask]
+        }
+
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        ZTest [unity_GUIZTestMode]
+        Blend SrcAlpha OneMinusSrcAlpha
+        ColorMask [_ColorMask]
+
+        Pass
+        {
+            Name "Outline"
+        
+        CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+
+            #include "UnityCG.cginc"
+            #include "UnityUI.cginc"
+
+            #pragma multi_compile __ UNITY_UI_CLIP_RECT
+            #pragma multi_compile __ UNITY_UI_ALPHACLIP
+
+            struct appdata_t
+            {
+                float4 vertex   : POSITION;
+                float4 color    : COLOR;
+                float2 texcoord : TEXCOORD0;
+                float4 tangent  : TANGENT;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex   : SV_POSITION;
+                fixed4 color    : COLOR;
+                float2 texcoord : TEXCOORD0;
+                float4 tangent  : TANGENT;
+                float4 worldPosition : TEXCOORD1;
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            fixed4 _Color;
+            fixed4 _TextureSampleAdd;
+            float4 _ClipRect;
+
+            v2f vert(appdata_t v)
+            {
+                v2f OUT;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
+                OUT.worldPosition = v.vertex;
+                OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
+
+                OUT.texcoord = v.texcoord;
+                OUT.tangent = v.tangent;
+
+                OUT.color = v.color * _Color;
+                return OUT;
+            }
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+            
+            float4 _OutlineColor;
+            int _OutlineWidth;
+            
+            fixed IsInRect(float2 pos, float4 clipRect)
+            {
+                pos = step(clipRect.xy, pos) * step(pos, clipRect.zw);
+                return pos.x * pos.y;
+            }
+            
+            fixed SampleAlpha(int index, v2f IN)
+            {
+                const fixed sinArray[12] = { 0, 0.5, 0.866, 1, 0.866, 0.5, 0, -0.5, -0.866, -1, -0.866, -0.5 };
+                const fixed cosArray[12] = { 1, 0.866, 0.5, 0, -0.5, -0.866, -1, -0.866, -0.5, 0, 0.5, 0.866 };
+                float2 pos = IN.texcoord + _MainTex_TexelSize.xy * float2(cosArray[index], sinArray[index]) * _OutlineWidth;
+                return IsInRect(pos, IN.tangent) * (tex2D(_MainTex, pos) + _TextureSampleAdd).w * _OutlineColor.w;
+            }
+
+            fixed4 frag(v2f IN) : SV_Target
+            {
+                half4 color = (tex2D(_MainTex, IN.texcoord) + _TextureSampleAdd) * IN.color;
+
+                #ifdef UNITY_UI_CLIP_RECT
+                color.a *= UnityGet2DClipping(IN.worldPosition.xy, _ClipRect);
+                #endif
+                
+                color.w *= IsInRect(IN.texcoord, IN.tangent);
+                half4 val = half4(_OutlineColor.x, _OutlineColor.y, _OutlineColor.z, 0);
+
+                val.w += SampleAlpha(0, IN);
+                val.w += SampleAlpha(1, IN);
+                val.w += SampleAlpha(2, IN);
+                val.w += SampleAlpha(3, IN);
+                val.w += SampleAlpha(4, IN);
+                val.w += SampleAlpha(5, IN);
+                val.w += SampleAlpha(6, IN);
+                val.w += SampleAlpha(7, IN);
+                val.w += SampleAlpha(8, IN);
+                val.w += SampleAlpha(9, IN);
+                val.w += SampleAlpha(10, IN);
+                val.w += SampleAlpha(11, IN);
+
+                color = step(0.0001, _OutlineWidth) * (val * (1.0 - color.a)) + (color * color.a);
+
+                #ifdef UNITY_UI_ALPHACLIP
+                clip (color.a - 0.001);
+                #endif
+
+                return color;
+            }
+        ENDCG
+        }
+    }
+}
